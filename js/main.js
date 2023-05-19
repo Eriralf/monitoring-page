677f769203225023cac1d40bf7cfe13b5e6977ae
Eriralf <eriral222@gmail.com>
2023-05-19 00:35:07 -0300

    create animations with scroll reveal for all page

--- js/main.js
+++ js/main.js
@@ -4,10 +4,31 @@ window.addEventListener('scroll', function () {
 });
 
 const progressBar = document.getElementById("progress-bar")
-
 window.addEventListener('scroll', () => {
      let height = document.body.scrollHeight - window.innerHeight;
      let scrollPosition = document.documentElement.scrollTop;
      let width = (scrollPosition / height) * 100;
      progressBar.style.width = `${width}%`;
 })
+
+const slideContainer = {
+     origin: "top",
+     distance: "56px",
+     duration: 2000,
+}
+const slideHeader = {
+     origin: "top",
+     distance: "56px",
+     duration: 3000,
+}
+const slideBox = {
+     origin: "top",
+     distance: "56px",
+     duration: 2000,
+     interval: 300,
+}
+ScrollReveal().reveal('.container', slideContainer);
+ScrollReveal().reveal('.nav-bar', slideHeader);
+ScrollReveal().reveal('.service-box-info', slideBox);
+ScrollReveal().reveal('.depoiments-box-info', slideBox);
+ScrollReveal().reveal('.complement-container', slideBox);
